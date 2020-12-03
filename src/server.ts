42269119d0ce537445992e633a8bbc3ffdd76f39
mccor2000 <roccm0200@gmail.com>
2020-12-03 15:44:24 +0700

    feat: setup server composer

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,21 @@
-import { Server, Request, ResponseToolkit } from '@hapi/hapi';
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const init = async () => {
-  const server: Server = new Server({
-    port: process.env.PORT || 3000,
-    host: 'localhost',
-  });
+import * as Manifest from "./config/manifest";
+import * as Glue from "glue";
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request: Request, h: ResponseToolkit) => {
-      return 'Hello world';
-    },
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/test',
-    handler: (request: Request, h: ResponseToolkit) => {
-      return 'Test route';
-    },
-  });
-
-  server.ext('onRequest', (request: Request, h: ResponseToolkit) => {
-    request.setUrl('/test');
-    return h.continue;
-  });
-
-  await server.start();
-  console.log(`Server is running on ${server.info.uri}`);
+const composerOptions = {
+  relativeTo: __dirname,
 };
 
-process.on('unhandledRejection', (err) => {
-  console.log(err);
-  process.exit(1);
-});
+(async function () {
+  try {
+    const server = await Glue.compose(Manifest.get("/"), composerOptions);
 
-init();
+    await server.start();
+    console.log(`Server is running at ${server.info.uri}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+})();
